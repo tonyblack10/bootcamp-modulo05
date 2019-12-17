cc476fd01447b86a53f10581ed3a0d4f87e04e31
Tony Costa <tonyblack10@gmail.com>
2019-12-16 22:40:23 -0300

    Updates

---
+++ precache-manifest.c457bffa91340fe6a035df11506ab778.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7db5c48c2b57f3edb7e02893d3cb942a",
+    "url": "/bootcamp-modulo05/index.html"
+  },
+  {
+    "revision": "b26b0553e990d22363f0",
+    "url": "/bootcamp-modulo05/static/js/2.9d35e109.chunk.js"
+  },
+  {
+    "revision": "0c559e1ae0a9426ead1485b442d61f0b",
+    "url": "/bootcamp-modulo05/static/js/2.9d35e109.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7e91d635aaeb2fe83aa3",
+    "url": "/bootcamp-modulo05/static/js/main.6085fee4.chunk.js"
+  },
+  {
+    "revision": "a76057c4981d9069cd28",
+    "url": "/bootcamp-modulo05/static/js/runtime-main.c396613b.js"
+  }
+]);
